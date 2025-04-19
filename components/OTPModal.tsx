fb6e4063c5e33b6e62154db20020f2796b61b705
Gaurang-0204 <143063021+Gaurang-0204@users.noreply.github.com>
2025-04-19 23:59:20 +0530

    Update OTPModal.tsx  changed routing

--- components/OTPModal.tsx
+++ components/OTPModal.tsx
@@ -44,7 +44,7 @@ const OtpModal = ({
 
       console.log({ sessionId });
 
-      if (sessionId) router.push("/");
+      if (sessionId) router.push("/root");
     } catch (error) {
       console.log("Failed to verify OTP", error);
     }
